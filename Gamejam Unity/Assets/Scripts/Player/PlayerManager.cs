7f6dba3e1ecc489631f232ae162318e29afcd4ad
CDreyer <cristianbr.dreyer@gmail.com>
2021-07-25 17:27:55 -0300

    player script added

---
+++ Gamejam Unity/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviourPunCallbacks
+{
+    public static PlayerManager instance;
+
+    public GameObject PlayerLook;
+    public GameObject DesktopCanvas;
+    public GameObject DoorWithPassword;
+
+    GameObject Crosshair;
+
+    public bool isMasterClient;
+    public bool PVisMine;
+    public string chatText;
+
+    ChatManager _chatManager;
+
+    private void Awake()
+    {
+        instance = this;
+
+        /*if(MenuController._CurrentPlayer == 0)
+        {
+            //this.photonView.name = "PlayerPresent";
+            GetComponent<FloorPuzzleController>().playerIsOnPresent = true;
+        }
+        else
+        {
+            //this.photonView.name = "PlayerFuture";
+            GetComponent<FloorPuzzleController>().playerIsOnPresent = false;
+        }*/
+
+    }
+    void Start()
+    {
+        if (!photonView.IsMine)
+        {
+            this.enabled = false;
+            transform.GetChild(0).transform.GetChild(0).gameObject.GetComponent<Camera>().enabled = false;
+            GetComponent<PlayerMovement>().enabled = false;
+        }
+        else
+        {
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                isMasterClient = true;
+                
+            }
+            else
+            {
+                isMasterClient = false;
+                
+            }
+                
+
+            instance = this;
+            PVisMine = true;
+
+           
+            Debug.Log(isMasterClient);
+        }
+
+        Crosshair = GameObject.Find("CrossHair");
+        
+    }
+
+    [SerializeField]float opacity = 0;
+    bool explosion = true;
+    void Update()
+    {
+  
+
+        if (GameObject.Find("Pause_Panel") != null)
+        {
+            GetComponent<PlayerMovement>().canWalk = false;
+            GetComponent<PlayerMovement>().canMoveCamera = false;
+            Crosshair.SetActive(false);
+        }
+        else
+        {
+            DesktopPuzzle();
+        }
+
+        //call te credits and icrease his opacity
+        if(GameObject.Find("END GAME FADE") != null)
+        {
+
+
+            if (GameObject.Find("END GAME FADE").activeInHierarchy)
+            {
+                
+
+                GameObject.Find("END GAME FADE").GetComponent<Image>().color = new Color(GameObject.Find("END GAME FADE").GetComponent<Image>().color.r, GameObject.Find("END GAME FADE").GetComponent<Image>().color.g,
+                    GameObject.Find("END GAME FADE").GetComponent<Image>().color.b, opacity);
+
+                opacity += 0.3f * Time.deltaTime;
+
+                if (explosion && opacity >= 0.8f)
+                {
+                    GameObject.Find("Explosion").GetComponent<AudioSource>().Play();
+                    explosion = false;
+                }
+                
+
+                // return to menu after presses left mouse button
+                if (Input.GetMouseButton(0))
+                {
+                    PhotonNetwork.Disconnect();
+                    SceneManager.LoadScene(0);
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                }
+
+                
+            }
+        }
+        
+
+    }
+
+    void DesktopPuzzle()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(PlayerLook.transform.position, PlayerLook.transform.TransformDirection(Vector3.forward), out hit))
+        {
+            if (Input.GetButtonDown("Fire1") && hit.collider.name == "PC_Raycast")
+            {
+                DesktopCanvas.SetActive(true);
+
+                if(MenuController._CurrentPlayer == 0)
+                {
+                    DoorWithPassword = GameObject.Find("Door0");
+                }
+                else
+                {
+                    DoorWithPassword = GameObject.Find("Door1");
+                }
+            }
+        }
+
+
+        // check if Desktop is enabled;
+        if (DesktopCanvas.activeInHierarchy)
+        {
+            GetComponent<PlayerMovement>().canWalk = false;
+            GetComponent<PlayerMovement>().canMoveCamera = false;
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            Crosshair.SetActive(false);
+        }
+        else
+        {
+            GetComponent<PlayerMovement>().canWalk = true;
+            GetComponent<PlayerMovement>().canMoveCamera = true;
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            Crosshair.SetActive(true);
+        }
+    }
+}
---
+++ Gamejam Unity/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using ExitGames.Client.Photon.StructWrapping;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variables
+
+    [Header("Permissions")]
+    public bool canWalk;
+    public bool canMoveCamera;
+
+    [Header("Parameters")]
+    public float movementSpeed = 400f;
+    public float cameraSpeed = 100f;
+
+    GameObject CameraObject;
+    Rigidbody rb;
+
+
+    #endregion
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        CameraObject = transform.GetChild(0).transform.GetChild(0).gameObject;
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;     //lock cursor in the center
+    }
+
+    [System.Obsolete]
+    void Update()
+    {
+
+
+        if (canWalk)
+            Movement();
+
+        if(canMoveCamera)
+            MoveCamera();
+
+        //if(transform.FindChild("ObjectPosition").childCount == 0)
+        //{
+            //canMoveCamera = true;
+        //}
+    }
+
+    #region Movement Control
+
+    void Movement()
+    {
+        float fowardMove = Input.GetAxisRaw("Vertical") * Time.deltaTime;
+        float sideMove = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+
+        Vector3 MoveVector = transform.right * sideMove + transform.forward * fowardMove;
+
+        rb.velocity = MoveVector.normalized * movementSpeed * Time.deltaTime + new Vector3(0, rb.velocity.y, 0);
+    
+    }
+
+    #endregion
+
+
+    #region Player Camera Controll
+
+    float mouseX, mouseY;
+    void MoveCamera()
+    {
+        mouseX += Input.GetAxis("Mouse X") * cameraSpeed * Time.deltaTime;
+        mouseY -= Input.GetAxis("Mouse Y") * cameraSpeed * Time.deltaTime;
+        mouseY = Mathf.Clamp(mouseY, -50, 60);
+        transform.rotation = Quaternion.Euler(0, mouseX, 0);
+
+        CameraObject.transform.localRotation = Quaternion.Euler(mouseY, 0, 0);
+
+    }
+
+    #endregion
+
+    
+}
